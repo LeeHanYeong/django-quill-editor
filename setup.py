7e7b668103899ced26648772402d10a45a1b1dbc
lhy <dev@lhy.kr>
2020-06-09 20:47:21 +0900

    Fixed an error that static files were not included in the distribution package

--- setup.py
+++ setup.py
@@ -9,18 +9,21 @@ VERSION = open(os.path.join(ROOT_DIR, 'version.txt')).read()
 setup(
     name='django-quill-editor',
     version=VERSION,
-    description='Integrate Quill editor with Django project.',
-    long_description=README,
-    long_description_content_type='text/markdown',
+    url='https://github.com/LeeHanYeong/django-quill-editor',
     author='Lee HanYeong',
     author_email='dev@lhy.kr',
     license='MIT',
+
+    description='Integrate Quill editor with Django project.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+
     packages=find_packages(exclude=['test*', 'sample']),
+    include_package_data=True,
     install_requires=[
         'django',
     ],
     python_requires=">3.5",
-    url='https://github.com/LeeHanYeong/django-quill-editor',
     zip_safe=True,
     classifiers=[
         'Framework :: Django',
