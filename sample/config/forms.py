8310eb8ac5c16db860230dfe1fc7fc9ce32f91ab
lhy <dev@lhy.kr>
2020-06-14 18:19:30 +0900

    Remove sample project

--- sample/config/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-from django_quill.forms import QuillFormField
-
-
-class QuillForm(forms.Form):
-    quill = QuillFormField()
--- sample/posts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import QuillPost
-
-
-@admin.register(QuillPost)
-class QuillPostAdmin(admin.ModelAdmin):
-    pass
