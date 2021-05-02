e6d16a5446ec7f8bd7a13830beb7720178366c72
Jonhnatha Trigueiro <joepreludian@gmail.com>
2021-05-01 22:20:18 -0300

    Update project in order to support multiple django Quills

--- django_quill/static/django_quill/django_quill.js
+++ django_quill/static/django_quill/django_quill.js
@@ -15,3 +15,58 @@ class QuillWrapper {
         });
     }
 }
+
+(function($) {
+
+    $(function(){
+        // Setting current enabled __prefix__ object as the one that will be cloned;
+        $('.django-quill-widget-container').each((idx, quill_container) => {
+            let quill_widget = $(quill_container).find('.django-quill-widget');
+            quill_widget = quill_widget[0];
+
+            //Injecting this logic inside the component so I will be able to know that future cloned items will inherit this data.
+            if (quill_widget.id.includes('__prefix__') === true) {
+                $(quill_widget).data('is_quill_cloned', '1');
+            }
+        });
+    });
+
+    // Using custom events from django
+    // https://docs.djangoproject.com/en/3.2/ref/contrib/admin/javascript/#inline-form-events
+    $(document).on('formset:added', function(event, $row, formsetName) {
+        //console.log('New item added', event, formsetName, $row);
+
+        $('.django-quill-widget-container').each((idx, quill_container) => {
+
+            let quill_widget = $(quill_container).find('.django-quill-widget');
+            quill_widget = quill_widget[0];
+
+            let quill_input = $(quill_container).find('.django-quill-widget-input');
+            quill_input = quill_input[0];
+
+            if (quill_widget.id.includes('__prefix__') === false) {
+                if ($(quill_widget).data('is_quill_enabled') !== '1') {
+                    if($(quill_widget).data('is_quill_cloned') == '1') {
+                        // Removing old toolbar since it cames from the Clone;
+                        $(quill_container).find('.ql-toolbar').remove();
+
+                        console.log(quill_widget.id, quill_input.id);
+                        new QuillWrapper(quill_widget.id, quill_input.id,{"theme": "snow", "modules": {"syntax": true, "toolbar": [[{"font": []}, {"header": []}, {"align": []}, "bold", "italic", "underline", "strike", "blockquote", {"color": []}, {"background": []}], ["code-block", "link", "image"], ["clean"]]}} );
+
+                        $(quill_widget).data('is_quill_enabled', '1');
+                    }
+                }
+            }
+
+        });
+
+        //if (formsetName == 'author_set') {
+        // Do something
+        //}
+    });
+
+    $(document).on('formset:removed', function(event, $row, formsetName) {
+        // Row removed
+    });
+
+})(django.jQuery);
